74f0a0751ecef3c4ebb72a7c516bfa2a25ac0cc5
stephen1521 <stephen.warnock@codeimmersives.com>
2023-03-04 13:36:39 -0500

    added back side of card and flipping of the card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 
@@ -12,34 +11,29 @@ function RMApi (props) {
 	useEffect(() => {
 		fetch(props.url)
 		  .then(res => res.json())
-		  .then(
-			(result) => {
-			  setIsLoaded(true);
-			  setItems(result);
-			},
-			(error) => {
-			  setIsLoaded(true);
-			  setError(error);
-			}
-		  )
-	  }, [])
-	  if (error) {
-		return <div>Error: {error.message}</div>;
-	  } else if (!isLoaded) {
-		return <div>Loading...</div>;
-	  } else {
-		// return (
-		//   <div>
-		// 	<Card character={items.results[0]}/>
-		//   </div>
-		// );
-		const cards = items.results.map((character, index) => {
+		  	.then(
+				(result) => {
+			  		setIsLoaded(true);
+			  		setItems(result);
+				},
+				(error) => {
+			  		setIsLoaded(true);
+			  		setError(error);
+				}
+		  	)	
+	}, [])
+	  	if (error) {
+			return <div>Error: {error.message}</div>;
+	  	} else if (!isLoaded) {
+			return <div>Loading...</div>;
+	  	} else {
+			const cards = items.results.map((character, index) => {
 			return (
 				<Card key={index} character={character}/>
 			)
-		})
-		return <div className='d-flex flex-wrap justify-content-center align-items-center gap-3 me-5 ms-5 mt-5'>{cards}</div>
-	  }
+			})
+			return <div className='d-flex flex-wrap justify-content-center align-items-center gap-3 me-5 ms-5 mt-5'>{cards}</div>
+	  	}
 }
 
 function App() {
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 function CardFront(props){
@@ -16,14 +16,38 @@ function CardFront(props){
 }
 
 function CardBack(props){
-
+    return (
+        <Card style={{ width: '12rem' }}>
+            <Card.Body>
+                <Card.Title>Extra Information:</Card.Title>
+                <Card.Text>Origin: {props.character.origin.name}</Card.Text>
+                <Card.Text>Location: {props.character.location.name}</Card.Text>
+                <Card.Text>Number of episodes: {props.character.episode.length}</Card.Text>
+            </Card.Body>
+        </Card>
+    )
 }
+
 
 function CardDisplay(props) {
     const { character } = props;
-    return (
-       <CardFront character={character}/>
-    )
+    const [flip, setFlip] = useState(false);
+    function handleToggle(){
+        setFlip((current) => !current);
+    };
+    if(!flip){
+        return (
+            <div onClick={handleToggle}>
+                <CardFront character={character}/>
+            </div>
+        )
+    } else if(flip){
+        return (
+            <div onClick={handleToggle}>
+                <CardBack character={character}/>
+            </div>
+        )
+    }
 }
 
 export default CardDisplay;
